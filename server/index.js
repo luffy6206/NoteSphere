aa7ec112e6bcfd45e5701acd61548743b8473595
ashish raj <ashishraj6206@gmail.com>
2026-04-26 10:35:37 +0530

    db connected and user schema designed

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv"
+import connectDB from "./utils/connectDB.js";
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get("/",(req,res)=>{
+    res.json({message: "examnotes AI backend running"})
+})
+
+app.listen(PORT,()=>{
+    console.log(`server running on port ${PORT}`)
+    connectDB()
+});
