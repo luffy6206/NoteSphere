dfd1ba12806fcaf3617aa90f5c5ddc116e7aba2f
ashish raj <ashishraj6206@gmail.com>
2026-04-30 00:28:03 +0530

    Updated gitignore and project files

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
-import express from "express";
+import express from "express"
 import dotenv from "dotenv"
-import connectDB from "./utils/connectDB.js";
-dotenv.config();
+import connectDB from "./utils/connectDB.js"
+dotenv.config()
 
 const app = express();
 const PORT = process.env.PORT || 5000;
